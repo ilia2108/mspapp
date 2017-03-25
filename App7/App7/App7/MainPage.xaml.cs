3f60dbf86176f865d272e53bc87cb28540a299c6
Max <max@sppro.ru>
2017-03-25 16:15:00 +0300

    pull

---
+++ App7/App7/App7/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using App7.ChildContent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            menuListView.ItemsSource=Enumerable.Range(1, 10).Select(i=>new MenuElement()
+            {
+                Text="Menu "+i,
+                PageType=i%2==0 ? typeof(View1) : typeof(View2)
+            });
+        }
+
+        private void menuListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var menuElement = e.SelectedItem as MenuElement;
+            ChildPagePresenter.Content = Activator.CreateInstance(menuElement.PageType) as View;
+        }
+    }
+
+    public class MenuElement
+    {
+        public string Text { get; set; }
+        public string ImageSource { get; set; }
+
+        public Type PageType { get; set; }
+    }
+}
